ddce5d696bde1da8f7ec8f7c24681272b7191c9c
LZpenguin <1255614802@qq.com>
2021-10-02 18:52:55 +0800

    add tag anim

--- code/assets/js/mask.js
+++ code/assets/js/mask.js
@@ -3,6 +3,7 @@ window.onload = function() {
   var maskn = document.querySelectorAll('.maskn');
   var btns = document.querySelectorAll('.btn');
   var postcards = document.querySelectorAll('.postcard');
+  var tags = document.querySelectorAll('.tag');
   var height = document.documentElement.clientHeight;
   var width = document.documentElement.clientWidth;
   console.log(maskn);
@@ -40,11 +41,36 @@ window.onload = function() {
       setTimeout(() => {
         maskn.forEach((item) => {
           item.style.transition = 'all 0.5s linear';
+          item.style.width = '12rem';
+          item.style.height = '12rem';
           item.style.transform = 'translate(-50%, -50%) rotate('+Math.random()*360+'deg) scale(1)';
           item.style.left = Math.random()*width + 'px';
           item.style.top = Math.random()*height + 'px';
+          
+        })
+      },0)
+      setTimeout(() => {
+        mask.style.visibility = 'hidden';
+      },800)
+    }
+  }
+  for(var i in tags) {
+    tags[i].onclick = function(e) {
+      mask.style.visibility = 'visible';
+      maskn.forEach((item) => {
+        item.style.transition = 'all 0s';
+        item.style.left = e.clientX + 'px';
+        item.style.top = e.clientY + 'px';
+      })
+      setTimeout(() => {
+        maskn.forEach((item) => {
+          item.style.transition = 'all 0.5s linear';
           item.style.width = '12rem';
           item.style.height = '12rem';
+          item.style.transform = 'translate(-50%, -50%) rotate('+Math.random()*360+'deg) scale(1)';
+          item.style.left = Math.random()*width + 'px';
+          item.style.top = Math.random()*height + 'px';
+          
         })
       },0)
       setTimeout(() => {
