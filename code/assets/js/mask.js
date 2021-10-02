70489d1dec7873ff70e9090c08b2c9ca57d1a63f
LZpenguin <1255614802@qq.com>
2021-10-02 18:44:39 +0800

    interesting styles

---
+++ code/assets/js/mask.js
@@ -0,0 +1,55 @@
+window.onload = function() {
+  var mask = document.querySelector('.mask');
+  var maskn = document.querySelectorAll('.maskn');
+  var btns = document.querySelectorAll('.btn');
+  var postcards = document.querySelectorAll('.postcard');
+  var height = document.documentElement.clientHeight;
+  var width = document.documentElement.clientWidth;
+  console.log(maskn);
+  for(var i in btns) {
+    btns[i].onclick = function(e) {
+      mask.style.visibility = 'visible';
+      maskn.forEach((item) => {
+        item.style.transition = 'all 0s';
+        item.style.left = e.clientX + 'px';
+        item.style.top = e.clientY + 'px';
+      })
+      setTimeout(() => {
+        maskn.forEach((item) => {
+          item.style.transition = 'all 0.5s linear';
+          item.style.transform = 'translate(-50%, -50%) rotate('+Math.random()*360+'deg) scale(1)';
+          item.style.left = Math.random()*width + 'px';
+          item.style.top = Math.random()*height + 'px';
+          item.style.width = '12rem';
+          item.style.height = '12rem';
+        })
+      },0)
+      setTimeout(() => {
+        mask.style.visibility = 'hidden';
+      },800)
+    }
+  }
+  for(var i in postcards) {
+    postcards[i].onclick = function(e) {
+      mask.style.visibility = 'visible';
+      maskn.forEach((item) => {
+        item.style.transition = 'all 0s';
+        item.style.left = e.clientX + 'px';
+        item.style.top = e.clientY + 'px';
+      })
+      setTimeout(() => {
+        maskn.forEach((item) => {
+          item.style.transition = 'all 0.5s linear';
+          item.style.transform = 'translate(-50%, -50%) rotate('+Math.random()*360+'deg) scale(1)';
+          item.style.left = Math.random()*width + 'px';
+          item.style.top = Math.random()*height + 'px';
+          item.style.width = '12rem';
+          item.style.height = '12rem';
+        })
+      },0)
+      setTimeout(() => {
+        mask.style.visibility = 'hidden';
+      },800)
+    }
+  }
+}
