df92cd198da6159d8fe3e0ce1916d094617e2cc2
LZpenguin <1255614802@qq.com>
2021-10-04 19:32:01 +0800

    adaptation

--- code/assets/js/mask.js
+++ code/assets/js/mask.js
@@ -4,11 +4,15 @@ window.onload = function() {
   var btns = document.querySelectorAll('.btn');
   var postcards = document.querySelectorAll('.postcard');
   var tags = document.querySelectorAll('.tag');
+  var menu = document.querySelector('.menu');
+  var aside = document.querySelector('.aside');
   var height = document.documentElement.clientHeight;
   var width = document.documentElement.clientWidth;
-  console.log(maskn);
   for(var i in btns) {
     btns[i].onclick = function(e) {
+      e.stopPropagation();
+      height = document.documentElement.clientHeight;
+      width = document.documentElement.clientWidth;
       mask.style.visibility = 'visible';
       maskn.forEach((item) => {
         item.style.transition = 'all 0s';
@@ -21,8 +25,13 @@ window.onload = function() {
           item.style.transform = 'translate(-50%, -50%) rotate('+Math.random()*360+'deg) scale(1)';
           item.style.left = Math.random()*width + 'px';
           item.style.top = Math.random()*height + 'px';
-          item.style.width = '12rem';
-          item.style.height = '12rem';
+          if(width>420) {
+            item.style.width = '12rem';
+            item.style.height = '12rem';
+          }else {
+            item.style.width = '6rem';
+            item.style.height = '6rem';
+          }
         })
       },0)
       setTimeout(() => {
@@ -32,6 +41,8 @@ window.onload = function() {
   }
   for(var i in postcards) {
     postcards[i].onclick = function(e) {
+      height = document.documentElement.clientHeight;
+      width = document.documentElement.clientWidth;
       mask.style.visibility = 'visible';
       maskn.forEach((item) => {
         item.style.transition = 'all 0s';
@@ -41,12 +52,16 @@ window.onload = function() {
       setTimeout(() => {
         maskn.forEach((item) => {
           item.style.transition = 'all 0.5s linear';
-          item.style.width = '12rem';
-          item.style.height = '12rem';
           item.style.transform = 'translate(-50%, -50%) rotate('+Math.random()*360+'deg) scale(1)';
           item.style.left = Math.random()*width + 'px';
           item.style.top = Math.random()*height + 'px';
-          
+          if(width>420) {
+            item.style.width = '12rem';
+            item.style.height = '12rem';
+          }else {
+            item.style.width = '6rem';
+            item.style.height = '6rem';
+          }
         })
       },0)
       setTimeout(() => {
@@ -56,6 +71,8 @@ window.onload = function() {
   }
   for(var i in tags) {
     tags[i].onclick = function(e) {
+      height = document.documentElement.clientHeight;
+      width = document.documentElement.clientWidth;
       mask.style.visibility = 'visible';
       maskn.forEach((item) => {
         item.style.transition = 'all 0s';
@@ -65,17 +82,27 @@ window.onload = function() {
       setTimeout(() => {
         maskn.forEach((item) => {
           item.style.transition = 'all 0.5s linear';
-          item.style.width = '12rem';
-          item.style.height = '12rem';
           item.style.transform = 'translate(-50%, -50%) rotate('+Math.random()*360+'deg) scale(1)';
           item.style.left = Math.random()*width + 'px';
           item.style.top = Math.random()*height + 'px';
-          
+          if(width>420) {
+            item.style.width = '12rem';
+            item.style.height = '12rem';
+          }else {
+            item.style.width = '6rem';
+            item.style.height = '6rem';
+          }
         })
       },0)
       setTimeout(() => {
         mask.style.visibility = 'hidden';
       },800)
     }
+  }
+  menu.onclick = function() {
+    aside.style.top = 0;
+  }
+  aside.onclick = function() {
+    aside.style.top = '-100%'
   }
 }
